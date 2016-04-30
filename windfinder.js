990fcda4b92325e96726bd8f127baf79556d2a95
Christopher <Christopher.Fenner@me.com>
2016-04-30 11:49:36 +0200

    add module content

--- windfinder.js
+++ windfinder.js
@@ -1,21 +1,46 @@
-Module.register("windfinder",{
-
+Module.register("windfinder", {
 	defaults: {
-		windUnit: 'kt'
-		prependString: 'System temperature: '
-		updateInterval: 60
+		// windUnit: 'kt',
+		updateInterval: 60,
+    animationSpeed: 1,
+		height: '213px',
+		width: '880px',
+		protocoll: 'https://',
+		apiBase: '.windfinder.com/widget/',
+		type: {
+			forecast: "forecast/"
+		},
+		location: "epplesee_rheinstetten"
 	},
 	start: function() {
-		this.temperature = 'fetching ...';
-		this.sendSocketNotification('CONNECT');
-setUpdateInterval(this.load, this.config.updateInterval);
+		// setUpdateInterval(function(){
+	  //   this.updateDom(this.config.animationSpeed * 1000);
+		// }.bind(this), this.config.updateInterval * 60 * 1000);
 	},
-
-	// Override dom generator.
 	getDom: function() {
 		var wrapper = document.createElement("div");
-var iframe = document.createElement('iFrame');
-		//wrapper.innerHTML = ;
+		var iframe = document.createElement("iFrame");
+		iframe.src = this.config. protocoll + config.language + this.config.apiBase
+			+ this.config.type.forecast
+			+ this.config.location
+			+ "?domain=localhost"
+			+ "&columns=3"
+			+ "&days=3"
+			+ "&show_day=1"
+			+ "&show_pressure=0"
+			+ "&show_clouds=0"
+			+ "&show_temperature=1"
+			+ "&show_rain=0"
+			+ "&show_wind=1"
+			+ "&show_waves=1"
+			+ "&unit_temperature=c"
+			+ "&unit_rain=mm"
+			+ "&unit_wind=kts"
+			+ "&unit_wave=m";
+		iframe.style.border = "none";
+		iframe.style.height = this.config.height;
+		iframe.style.width = this.config.width;
+		wrapper.appendChild(iframe);
 		return wrapper;
 	}
 });
